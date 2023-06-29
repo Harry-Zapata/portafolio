a501b70aa466d5ea116dacfd373ecf4a9e5066cb
Harry-Zapata <newdescargas21@gmail.com>
2023-06-29 16:26:24 -0500

    imagen principal dinamica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,42 +6,52 @@ import { PrincipalService } from './services/principal.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Portafolio';
-  constructor(private _principalService:PrincipalService ){}
+  bgStyle!:any;
+  constructor(private _principalService: PrincipalService) { }
   ngOnInit(): void {
     this.obtenerData();
   }
-  obtenerData(){
-    this._principalService.getPrincipal().subscribe(data=>{
-      this.data=data[0];
-    },error=>{
+  obtenerData() {
+    this._principalService.getPrincipal().subscribe(data => {
+      this.data = data[0];
+      this.bgStyle = {
+        'animation': '40s linear 0s normal none infinite running bgScroll',
+        'background': `black url(${data[0].image.secure_url}) scroll center top`,
+        'background-repeat': 'no-repeat',
+        'background-size': 'cover'
+      }
+    }, error => {
       console.log(error);
     })
-    this._principalService.getRedes().subscribe(data=>{
-      this.redes=data;
-    },error=>{
+    this._principalService.getRedes().subscribe(data => {
+      this.redes = data;
+    }, error => {
       console.log(error);
     })
   }
-  
-  data={
-    nombre:"",
-    linkCV:"",
-    profesion:""
+  data = {
+    nombre: "",
+    linkCV: "",
+    profesion: "",
+    image: {
+      public_id: "",
+      secure_url: ""
+    }
   };
-  redes=[{red:"",link:""}]
+  redes = [{ red: "", link: "" }]
   proyecto = {
     nombre: "",
     categoria: "",
     cliente: "",
     rol: "",
-    fecha:"",
+    fecha: "",
     description: "",
-    link:"",
+    link: "",
     imagenes: []
   };
-  
+
   procesaPropagar(objProyecto: any) {
     this.proyecto = objProyecto;
   }
